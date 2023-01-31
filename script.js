b1daf1ca7b276134697d40c9d2ed61a4df128065
[anousheh21] <[anousheh21@gmail.com]>
2023-01-31 06:11:05 +0000

    Validates whether input field has been filled

--- script.js
+++ script.js
@@ -4,24 +4,34 @@ const button = document.querySelector(".submit");
 const answer = document.querySelector(".answer");
 //assigned the const 'question' to the form element
 let question = document.querySelector(".question");
+//assigned the const questionOutput to the empty p with class questionOutput
 const questionOutput = document.querySelector(".questionOutput");
+//assigned const userInput to the input text field
 let userInput = document.getElementById("userInput");
+//created an array with answers to the question
 const outputs = ['yes', 'no'];
+
 
 //event listener, listens for when the submit button is clicked
 button.addEventListener('click', () => {
-    //p tag is cleared
-    answer.textContent = '';
-    textValue();
-    //question.reset();
-    //output is assigned a random array item from the outputs array
-    let output = randomOutput();
-    //after 100 milliseconds, the p tag gets filled with the output, and displays on the screen
-    setTimeout(() => {answer.textContent = output;}, 10);
-    //after 100 milliseconds, the form 'reset' so will become empty, as opposed to being filled with user text
-    setTimeout(() => {question.reset()}, 100);
+    //clears printed question
+    questionOutput.textContent = '';
+    //if statement to validate whether the input field contains content
+    if (userInput.value == '') {
+        answer.textContent = 'Please Enter a Question...'
+    } else {
+        //clears answer
+        answer.textContent = '';
+        textValue();
+        //question.reset();
+        //output is assigned a random array item from the outputs array
+        let output = randomOutput();
+        //after 100 milliseconds, the p tag gets filled with the output, and displays on the screen
+        setTimeout(() => {answer.textContent = output;}, 10);
+        //after 100 milliseconds, the form 'reset' so will become empty, as opposed to being filled with user text
+        setTimeout(() => {question.reset()}, 100);
+        }
 });
-
 
 //function to randomly choose an item from the outputs array
 function randomOutput() {
